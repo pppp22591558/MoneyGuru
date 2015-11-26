604b789b2c758c070c2414623809f695bdf77625
Joshua <joshuacfang@gmail.com>
2015-11-26 19:18:08 +0800

    Initial Commit

---
+++ Money Guru/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Money Guru
+//
+//  Created by Joshua Fang on 11/26/15.
+//  Copyright © 2015 Joshua Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
